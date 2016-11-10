ad141c532ae9c017a53a19181a43789e8fbc9178
Saad Karim <skarim@us.ibm.com>
2016-11-10 11:42:23 -0500

    Testcases added to support better test coverage
    More testcases have been added for registering and
    enrolling a user, and also for util functions to provide
    better test coverage. Make file now checks to see
    if there is 75% test coverage for each package. If not,
    it will return appropriate message.
    
    Code was also reorganized and directory structure was changed.
    
    https://jira.hyperledger.org/browse/FAB-862
    
    Change-Id: I82c0265fd3a2bbae4c2b9f2145234db818cbb8cb
    Signed-off-by: Saad Karim <skarim@us.ibm.com>

--- util/cryptoUtil_test.go
+++ util/cryptoUtil_test.go
@@ -17,7 +17,6 @@ limitations under the License.
 package util
 
 import (
-
 	"io/ioutil"
 	"math/big"
 	"testing"
@@ -29,11 +28,11 @@ import (
 func TestRSASignAndVerify(t *testing.T) {
 	privKeyBuff, err := ioutil.ReadFile("../testdata/privateKey.pem")
 	if err != nil {
-		t.Fatalf("Unable to read private key PEM from file: ", err)
+		t.Fatalf("Unable to read private key PEM from file: %s", err)
 	}
 	rsaPrivateKey, err := GetPrivateKey(string(privKeyBuff))
 	if err != nil {
-		t.Fatalf("Unable to get private key: ", err)
+		t.Fatalf("Unable to get private key: %s", err)
 	}
 	pubKeyBuff, err := ioutil.ReadFile("../testdata/publicKey.pem")
 	if err != nil {
@@ -41,7 +40,7 @@ func TestRSASignAndVerify(t *testing.T) {
 	}
 	rsapublicKey, err := GetPublicKey(string(pubKeyBuff))
 	if err != nil {
-		t.Fatalf("Unable to get RSA public key: ", err)
+		t.Fatalf("Unable to get RSA public key: %s", err)
 	}
 	jsonString := ConvertJSONFileToJSONString("../testdata/TCertRequest.json")
 	message := []byte(jsonString)
@@ -110,8 +109,16 @@ func TestECMessageAltered(t *testing.T) {
 	}
 }
 
-/* TODO: Fix
-func TestCertExpiry(t *testing.T) {
+// func TestCertExpiry(t *testing.T) {
+// 	jsonString := ConvertJSONFileToJSONString("../testdata/TCertRequest.json")
+// 	signatureJSON := ConvertJSONFileToJSONString("../testdata/Signature.json")
+// 	isVerfied := VerifyMessage(jsonString, SignECMessage(jsonString, signatureJSON))
+// 	if isVerfied == false {
+// 		t.Fatalf("Verification failed due to certificate expired")
+// 	}
+// }
+
+func TestVerifyMessage(t *testing.T) {
 	jsonString := ConvertJSONFileToJSONString("../testdata/TCertRequest.json")
 	signatureJSON := ConvertJSONFileToJSONString("../testdata/Signature.json")
 	isVerfied := VerifyMessage(jsonString, SignECMessage(jsonString, signatureJSON))
@@ -119,7 +126,6 @@ func TestCertExpiry(t *testing.T) {
 		t.Fatalf("Verification failed due to certificate expired")
 	}
 }
-*/
 
 func TestGenNumber(t *testing.T) {
 	var numlen int64
