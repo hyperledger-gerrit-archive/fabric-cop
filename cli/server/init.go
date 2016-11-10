efd248d8ff34e2a377acb5a0b3d6a054c30896df
pl <intelmind787@gmail.com>
2016-11-10 12:24:39 -0500

    FAB-449 improved test coverage: cop server init CSRJSON
    Added "cop server init" command to generate private key and certificate.
    After executing "cop server init csr.json",
    server-cert.pem and server-key.pem are written to $COP_HOME directory.
    Test cases are in init_test.go.
    
    Change-Id: I301af097f6472a729472875e558930f821c78d01
    Signed-off-by: pl <intelmind787@gmail.com>

---
+++ cli/server/init.go
@@ -0,0 +1,119 @@
+/*
+Copyright IBM Corp. 2016 All Rights Reserved.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+		 http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package server
+
+import (
+	"encoding/json"
+	"errors"
+	"io/ioutil"
+	"os"
+	"path/filepath"
+
+	"github.com/cloudflare/cfssl/cli"
+	"github.com/cloudflare/cfssl/cli/serve"
+	"github.com/cloudflare/cfssl/csr"
+	"github.com/cloudflare/cfssl/initca"
+	"github.com/cloudflare/cfssl/log"
+)
+
+var initUsageText = `cop server init CSRJSON -- generates a new private key and self-signed certificate
+Usage:
+        cop server init CSRJSON
+Arguments:
+        CSRJSON:    JSON file containing the request, use '-' for reading JSON from stdin
+Flags:
+`
+
+var initFlags = []string{"remote", "u"}
+
+// InitCommand calls the command to geneates a private key and self-signed certificate
+func InitCommand() {
+	cmds := map[string]*cli.Command{
+		"init": InitServerCommand,
+	}
+	// Set the authentication handler
+	serve.SetWrapHandler(NewAuthWrapper)
+	// Add the "register" route/endpoint
+	//	serve.SetEndpoint("register", NewRegisterHandler)
+	// Add the "enroll" route/endpoint
+	//	serve.SetEndpoint("enroll", NewEnrollHandler)
+
+	// If the CLI returns an error, exit with an appropriate status code.
+	err := cli.Start(cmds)
+	if err != nil {
+		os.Exit(1)
+	}
+}
+
+// initMain creates the private key and self-signed certificate needed to start COP Server
+func initMain(args []string, c cli.Config) (err error) {
+	csrFile, args, err := cli.PopFirstArgument(args)
+	if err != nil {
+		return errors.New(err.Error())
+	}
+
+	csrFileBytes, err := cli.ReadStdin(csrFile)
+	if err != nil {
+		return errors.New(err.Error())
+	}
+
+	req := csr.CertificateRequest{
+		KeyRequest: csr.NewBasicKeyRequest(),
+	}
+	err = json.Unmarshal(csrFileBytes, &req)
+	if err != nil {
+		return errors.New(err.Error())
+	}
+
+	c = cli.Config{
+		IsCA: true,
+	}
+
+	var key, cert []byte
+	cert, _, key, err = initca.New(&req)
+	if err != nil {
+		return errors.New(err.Error())
+	}
+
+	s := new(Server)
+	COPHome, err := s.CreateHome()
+	if err != nil {
+		return errors.New(err.Error())
+	}
+	certerr := ioutil.WriteFile(COPHome+"/server-cert.pem", cert, 0755)
+	if certerr != nil {
+		log.Fatal("Error writing server-cert.pem to $COPHome directory")
+	}
+	keyerr := ioutil.WriteFile(COPHome+"/server-key.pem", key, 0755)
+	if keyerr != nil {
+		log.Fatal("Error writing server-key.pem to $COPHome directory")
+	}
+
+	return nil
+}
+
+// Init takes a CSRJSON file and outputs private key and self-signed cert
+func Init(dir string) {
+	osArgs := os.Args
+	CSRJSON := filepath.Join(dir, "csr_ecdsa.json")
+	os.Args = []string{"server", "init", CSRJSON}
+	InitCommand()
+	os.Args = osArgs
+}
+
+// InitServerCommand assembles the definition of Command 'genkey -initca CSRJSON'
+var InitServerCommand = &cli.Command{UsageText: initUsageText, Flags: initFlags, Main: initMain}
