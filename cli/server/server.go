aaa999dfb7606d08ebb6b48eef6347685aee89d3
Saad Karim <skarim@us.ibm.com>
2016-11-18 10:13:23 -0500

    Run the COP server in a cluster (Postgres)
    Run the COP server in a cluster sharing the same DB.
    Postgres support added in preparation for clustering. Readme
    updated with instruction on how to setup a cluster
    environment.
    
    https://jira.hyperledger.org/browse/FAB-113
    https://jira.hyperledger.org/browse/FAB-1134
    
    Change-Id: I0f835e543ead77c54ad6b25d6e078178c6990ee7
    Signed-off-by: Saad Karim <skarim@us.ibm.com>

--- cli/server/server.go
+++ cli/server/server.go
@@ -26,6 +26,7 @@ import (
 	"github.com/cloudflare/cfssl/cli/sign"
 	"github.com/cloudflare/cfssl/log"
 	cop "github.com/hyperledger/fabric-cop/api"
+	"github.com/jmoiron/sqlx"
 )
 
 // Usage text of 'cfssl serve'
@@ -105,8 +106,13 @@ func (s *Server) CreateHome() (string, error) {
 }
 
 // BootstrapDB loads the database based on config file
-func (s *Server) BootstrapDB(cfg *Config) error {
+func (s *Server) BootstrapDB(db *sqlx.DB) error {
 	log.Debug("Bootstrap DB")
+
+	CFG.DB = db
+	CFG.DBAccessor = NewDBAccessor()
+	CFG.DBAccessor.SetDB(db)
+
 	b := BootstrapDB()
 	b.PopulateGroupsTable()
 	b.PopulateUsersTable()
@@ -140,10 +146,6 @@ func startMain(args []string, c cli.Config) error {
 		return err
 	}
 
-	cfg.DB = db
-	cfg.DBAccessor = NewDBAccessor()
-	cfg.DBAccessor.SetDB(db)
-
 	var cfsslCfg cli.Config
 	cfsslCfg.CAFile = cfg.CACert
 	cfsslCfg.CAKeyFile = cfg.CAKey
@@ -154,7 +156,7 @@ func startMain(args []string, c cli.Config) error {
 	}
 	cfg.Signer = mySigner
 
-	s.BootstrapDB(cfg)
+	s.BootstrapDB(db)
 
 	return serve.Command.Main(args, c)
 }
