96c559cd773cc043562d60bc8a2a647709fd7fa8
ASHUTOSH KUMAR <ashutosh_kumar@hotmail.com>
2016-11-29 14:39:46 -0500

    FAB-266 TCert lib Partial impl.
    The implementation is non-HSM friendly where TCerts are generated
    based on Key Derivation function.This implements FAB-873 and
    FAB-876.
    
    FAB-873 Crypto Support for TCert Genertion.
         https://jira.hyperledger.org/browse/FAB-873
    
    FAB-876 As an application developer , I want to generate TCert locally
    based on ECert that I have or got from CoP sevice
         https://jira.hyperledger.org/browse/FAB-876
    
    Support for Elliptic Curve and RSA for TCert. Support for TCert
    Generation locally given all paramters necessary to generate TCert is
    being passed from calling application.This will be extended to support
    TCert on COP Server in next code drop.
    
    Change-Id: I904e5684d8a66e100f13ddd349399ebb86601631
    Signed-off-by: ASHUTOSH KUMAR <ashutosh_kumar@hotmail.com>

---
+++ lib/crypto/certificate.go
@@ -0,0 +1,172 @@
+/*
+Copyright IBM Corp. 2016 All Rights Reserved.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+		 http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package crypto
+
+import (
+	"crypto/rand"
+	"crypto/x509"
+	"crypto/x509/pkix"
+	"math/big"
+	"time"
+
+	"github.com/cloudflare/cfssl/log"
+)
+
+//CertificateSpec defines structure for Certificate template
+type CertificateSpec struct {
+	commonName       string
+	serialNumber     *big.Int
+	pub              interface{}
+	usage            x509.KeyUsage
+	NotBefore        time.Time
+	NotAfter         time.Time
+	ext              *[]pkix.Extension
+	country          string
+	State            string
+	locality         string
+	Organization     string
+	OrganizationUnit string
+}
+
+//NewCertificateFromSpec generates cerficate based on template passed
+func NewCertificateFromSpec(spec *CertificateSpec) ([]byte, error) {
+
+	var isCA = false
+
+	tmpl := x509.Certificate{
+		SerialNumber: spec.GetSerialNumber(),
+		Subject: pkix.Name{
+			CommonName:         spec.GetCommonName(),
+			Organization:       []string{spec.GetOrganization()},
+			OrganizationalUnit: []string{spec.GetOrganizationalUnit()},
+			Province:           []string{spec.GetState()},
+			Country:            []string{spec.GetCountry()},
+			Locality:           []string{spec.GetLocality()},
+		},
+		NotBefore: spec.GetNotBefore(),
+		NotAfter:  spec.GetNotAfter(),
+
+		SubjectKeyId:       *spec.GetSubjectKeyID(),
+		SignatureAlgorithm: spec.GetSignatureAlgorithm(),
+		KeyUsage:           spec.GetUsage(),
+
+		BasicConstraintsValid: true,
+		IsCA: isCA,
+	}
+
+	if len(*spec.GetExtensions()) > 0 {
+		tmpl.Extensions = *spec.GetExtensions()
+		tmpl.ExtraExtensions = *spec.GetExtensions()
+	}
+
+	pub := spec.GetPublicKey()
+
+	privateKey, certificate, err := GetCAKeyAndCert()
+	if err != nil {
+		log.Error("Problem in getting CA certificat and Key object generation")
+		return nil, err
+	}
+	raw, err := x509.CreateCertificate(rand.Reader, &tmpl, certificate, pub, privateKey)
+	log.Debug("Certificate Created")
+	if err != nil {
+		log.Error("Certificate Generation failed")
+		return nil, err
+	}
+
+	return raw, nil
+}
+
+// GetCommonName returns the spec's Common Name field/value
+//
+func (spec *CertificateSpec) GetCommonName() string {
+	return spec.commonName
+}
+
+// GetSerialNumber returns the spec's Serial Number field/value
+//
+func (spec *CertificateSpec) GetSerialNumber() *big.Int {
+	return spec.serialNumber
+}
+
+// GetPublicKey returns the spec's Public Key field/value
+//
+func (spec *CertificateSpec) GetPublicKey() interface{} {
+	return spec.pub
+}
+
+// GetUsage returns the spec's usage (which is the x509.KeyUsage) field/value
+//
+func (spec *CertificateSpec) GetUsage() x509.KeyUsage {
+	return spec.usage
+}
+
+// GetNotBefore returns the spec NotBefore (time.Time) field/value
+//
+func (spec *CertificateSpec) GetNotBefore() time.Time {
+	return spec.NotBefore
+}
+
+// GetNotAfter returns the spec NotAfter (time.Time) field/value
+//
+func (spec *CertificateSpec) GetNotAfter() time.Time {
+	return spec.NotAfter
+}
+
+// GetOrganization returns the spec's Organization field/value
+//
+func (spec *CertificateSpec) GetOrganization() string {
+	return spec.Organization
+}
+
+// GetCountry returns the spec's Country field/value
+//
+func (spec *CertificateSpec) GetCountry() string {
+	return spec.country
+}
+
+// GetSubjectKeyID returns the spec's subject KeyID
+//
+func (spec *CertificateSpec) GetSubjectKeyID() *[]byte {
+	return &[]byte{1, 2, 3, 4}
+}
+
+// GetSignatureAlgorithm returns the X509.SignatureAlgorithm field/value
+//
+func (spec *CertificateSpec) GetSignatureAlgorithm() x509.SignatureAlgorithm {
+	return x509.ECDSAWithSHA384
+}
+
+// GetExtensions returns the sepc's extensions
+//
+func (spec *CertificateSpec) GetExtensions() *[]pkix.Extension {
+	return spec.ext
+}
+
+//GetLocality returs subject's locality
+func (spec *CertificateSpec) GetLocality() string {
+	return spec.locality
+}
+
+//GetOrganizationalUnit returns subject's OrganizationalUNIT
+func (spec *CertificateSpec) GetOrganizationalUnit() string {
+	return spec.OrganizationUnit
+}
+
+//GetState returns subejct's state
+func (spec *CertificateSpec) GetState() string {
+	return spec.State
+}
