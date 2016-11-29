303bded2f56701472d6f23d42caacf93cf148819
ASHUTOSH KUMAR <ashutosh_kumar@hotmail.com>
2016-11-29 14:39:46 -0500

    FAB-266 TCert lib Partial impl.
    The implementation is non-HSM friendly where TCerts are generated
    based on Key Derivation function.This implements FAB-873 and
    FAB-876.
    
    FAB-873 Crypto Support for TCert Genertion.
         https://jira.hyperledger.org/browse/FAB-873
    
    FAB-876 As an application developer , I want to generate TCert locally
    based on ECert that I have or got from CoP sevice
         https://jira.hyperledger.org/browse/FAB-876
    
    Support for Elliptic Curve and RSA for TCert. Support for TCert
    Generation locally given all paramters necessary to generate TCert is
    being passed from calling application.This will be extended to support
    TCert on COP Server in next code drop.
    
    Change-Id: I904e5684d8a66e100f13ddd349399ebb86601631
    Signed-off-by: ASHUTOSH KUMAR <ashutosh_kumar@hotmail.com>

---
+++ lib/tcert/tcertrespmsg.go
@@ -0,0 +1,47 @@
+/*
+Copyright IBM Corp. 2016 All Rights Reserved.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+		 http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package tcert
+
+import (
+	"math/big"
+	"time"
+)
+
+//CreateSetResp  Message contains TCert Responses
+type CreateSetResp struct {
+	Certs *CertSet `json:"TCertSet,omitempty"`
+}
+
+//CertSet contains tcer set
+type CertSet struct {
+	ID    *big.Int  `json:"id,omitempty"`
+	TS    time.Time `json:"ts,omitempty"`
+	Key   []byte    `json:"key,omitempty"`
+	Certs []TCert   `json:"TCerts,omitempty"` //Base64 encoded string
+}
+
+//TCert structure contains TCert structure
+type TCert struct {
+	Cert []byte            `json:"TCert,omitempty"` //base64 encoded string
+	Keys map[string][]byte `json:"keys,omitempty"`  //base64 encoded string as value
+}
+
+//Key contains TCert Key and Certififate
+type Key struct {
+	privateKey  []byte
+	certificate []byte
+}
